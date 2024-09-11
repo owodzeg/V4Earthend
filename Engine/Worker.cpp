d37edbde34fd058afe37b0bdc488bb67b98e857c
owocek <owocektv@gmail.com>
2024-09-12 01:24:36 +0200

    greatly expand network functions

--- Engine/Worker.cpp
+++ Engine/Worker.cpp
@@ -1,15 +1,34 @@
 #include "Worker.h"
+#include "Func.h"
 #include <spdlog/spdlog.h>
 #include <fstream>
+#include <chrono>
+#include "ResourceManager.h"
+
+using namespace std::chrono;
 
 Worker::Worker()
 {
 
 }
 
+#ifdef __linux__
+void Worker::platformSpecific()
+{
+    platform = "linux";
+}
+#else
+void Worker::platformSpecific()
+{
+    platform = "windows";
+}
+#endif
+
+
 void Worker::init()
 {
     SPDLOG_INFO("Worker is alive!");
+    platformSpecific();
 
     while(active)
         listen();
@@ -18,19 +37,45 @@ void Worker::init()
 void Worker::getAvailableServers()
 {
     std::string listUrl = "https://dl.patafourgame.com/earthend/essentials/serverList.txt";
+    std::string s_servers;
 
     std::ifstream backup("backupServerList.txt");
     if(backup.good())
     {
         SPDLOG_INFO("Reading servers from backup");
     }
-
-    auto data = downloadFromUrl(listUrl);
+    else
+    {
+        auto data = downloadFromUrl(listUrl);
+        s_servers = std::string(data.begin(), data.end());
+        servers = Func::Split(s_servers, '\n');
+    }
 }
 
 void Worker::testConnection()
 {
+    std::vector<std::pair<std::string, int>> times;
 
+    for(auto server : servers)
+    {
+        std::pair<std::string, int> responseTime;
+
+        responseTime.first = server;
+        responseTime.second = testUrl(server);
+
+        times.push_back(responseTime);
+    }
+
+    std::sort(times.begin(), times.end(), [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+        return a.second < b.second;
+    });
+
+    for(auto x : times)
+    {
+        SPDLOG_INFO("Server: {}, Response: {}ms", x.first, x.second);
+    }
+
+    closestServer = times[0].first;
 }
 
 size_t Worker::WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output, int dl_id) {
@@ -38,23 +83,58 @@ size_t Worker::WriteCallback(void* contents, size_t size, size_t nmemb, std::str
     output->append((char*)contents, totalSize);
     return totalSize;
 }
+int Worker::ProgressCallback(void* clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow) {
+    Worker* worker = static_cast<Worker*>(clientp); // Cast clientp to Worker*
 
-int Worker::ProgressCallback(void* clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow)
-{
     if (dltotal > 0) {
         double progress = (double)dlnow / (double)dltotal * 100;
-        SPDLOG_INFO("Download Progress: {:.2f}%", progress);
+        worker->currentDLProgress = dlnow;      // Store dlnow in the member variable
+        worker->currentDLTotal = dltotal;  // Store dltotal in the member variable
+        worker->dl_buffer[worker->dl_buffer.size()-1].progress = worker->currentDLProgress;
+        worker->dl_buffer[worker->dl_buffer.size()-1].size = worker->currentDLTotal;
+        //SPDLOG_INFO("Download Progress: {:.2f}%", progress);
     } else {
-        SPDLOG_INFO("Download Progress: 0%");
+        //SPDLOG_INFO("Download Progress: 0%");
     }
     return 0; // Returning 0 tells libcurl to continue downloading
 }
 
-std::vector<char> Worker::downloadFromUrl(std::string& url)
+int Worker::testUrl(std::string& url)
+{
+    auto start = high_resolution_clock::now();
+    std::string responseString;
+
+    // Initialize CURL
+    CURL* curl = curl_easy_init();
+
+    if (curl) {
+        CURLcode res;
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Worker::WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseString);
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+        currentDLProgress = 0;
+        currentDLTotal = 0;
+    }
+
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    return duration.count();
+}
+
+std::vector<char> Worker::downloadFromUrl(std::string url, bool addToBuffer)
 {
     SPDLOG_INFO("Attempting to download {}", url);
 
     std::string responseString;
+    File newFile;
+
+    dl_buffer.push_back(newFile);
 
     // Initialize CURL
     CURL* curl = curl_easy_init();
@@ -70,6 +150,7 @@ std::vector<char> Worker::downloadFromUrl(std::string& url)
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseString);
         // Set progress function
         curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, Worker::ProgressCallback);
+        curl_easy_setopt(curl, CURLOPT_XFERINFODATA, this);
 
         // Enable progress meter (since CURLOPT_NOPROGRESS is disabled by default in newer versions)
         curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
@@ -80,11 +161,81 @@ std::vector<char> Worker::downloadFromUrl(std::string& url)
         // Check for errors
         if (res != CURLE_OK)
             SPDLOG_ERROR("curl_easy_perform() failed: {}", curl_easy_strerror(res));
-        else
-            SPDLOG_INFO("Response: {}", responseString);
 
         // Clean up CURL
         curl_easy_cleanup(curl);
+    }
+
+    if(addToBuffer)
+    {
+        files.push_back(std::vector<char>(responseString.begin(), responseString.end()));
+    }
+
+    return std::vector<char>(responseString.begin(), responseString.end());
+}
+
+std::vector<char> Worker::downloadFromUrlPost(std::string url, std::vector<std::string> postParams, std::vector<std::string> postValues, bool addToBuffer)
+{
+    SPDLOG_INFO("Attempting to download from {}", url);
+
+    std::ostringstream postStream;
+
+    // Initialize CURL
+    CURL* curl = curl_easy_init();
+
+    if(postParams.size() > 0)
+    {
+        for(auto i=0; i<postParams.size(); i++)
+        {
+            SPDLOG_INFO("Post param {} value {}", postParams[i], postValues[i]);
+
+            if(i != 0)
+                postStream << "&";
+
+            postStream << postParams[i] << "=" << curl_easy_escape(curl, postValues[i].c_str(), postValues[i].length());
+        }
+    }
+
+    std::string post = postStream.str();
+
+    std::string responseString;
+    File newFile;
+
+    dl_buffer.push_back(newFile);
+
+    if (curl) {
+        CURLcode res;
+
+        // Set the URL for the HTTPS request
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post.c_str());
+
+        // Use a callback function to capture the response data
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Worker::WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseString);
+        // Set progress function
+        curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, Worker::ProgressCallback);
+        curl_easy_setopt(curl, CURLOPT_XFERINFODATA, this);
+
+        // Enable progress meter (since CURLOPT_NOPROGRESS is disabled by default in newer versions)
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
+
+        // Perform the request
+        res = curl_easy_perform(curl);
+
+        // Check for errors
+        if (res != CURLE_OK)
+            SPDLOG_ERROR("curl_easy_perform() failed: {}", curl_easy_strerror(res));
+
+        // Clean up CURL
+        curl_easy_cleanup(curl);
+    }
+
+    //SPDLOG_INFO("RECEIVED: {}", responseString);
+
+    if(addToBuffer)
+    {
+        files.push_back(std::vector<char>(responseString.begin(), responseString.end()));
     }
 
     return std::vector<char>(responseString.begin(), responseString.end());
@@ -106,9 +257,27 @@ void Worker::listen()
             break;
         }
 
-        case GET_SERVERS: {
+        case INIT_FIRSTRUN: {
             busy = true;
             getAvailableServers();
+            testConnection();
+            downloadFromUrl(closestServer+"earthend/essentials/p4kakupop-pro.ttf", true);
+            auto img_data = downloadFromUrl(closestServer+"earthend/essentials/message.png");
+            sf::Image img;
+            img.loadFromMemory(img_data.data(), img_data.size());
+            ResourceManager::getInstance().loadImageAsSprite("resources/graphics/ui/dialog/message.png", img);
+            worked = true;
+            break;
+        }
+
+        case DOWNLOAD_EARTHEND: {
+            busy = true;
+            auto v = downloadFromUrlPost(closestServer+"getversion.php", {"product_id"}, {"earthend"});
+            std::string version = std::string(v.begin(), v.end());
+            SPDLOG_INFO("Current Earthend version {}", version);
+            auto f = downloadFromUrlPost(closestServer+"getfiles.php",{"product_id", "product_ver", "product_platform"}, {"earthend", version, platform});
+            std::string content = std::string(f.begin(), f.end());
+            SPDLOG_INFO("Files: {}", content);
             worked = true;
             break;
         }
@@ -116,6 +285,8 @@ void Worker::listen()
 
     if(worked)
         myAction = IDLE;
+
+    std::this_thread::sleep_for(100ms);
 }
 
 bool Worker::isBusy()
--- Engine/Worker.h
+++ Engine/Worker.h
@@ -10,24 +10,43 @@ public:
     std::vector<std::string> servers;
     std::string closestServer = "";
 
+    struct File
+    {
+        std::vector<char> buffer;
+        int size = 0;
+        int progress = 0;
+    };
+
+    std::vector<File> dl_buffer;
+    std::vector<std::vector<char>> files;
+
     bool active = true;
     bool busy = false;
+
+    float currentDLTotal = 0;
+    float currentDLProgress = 0;
 
     enum Action
     {
         IDLE = 0,
-        GET_SERVERS = 1
+        INIT_FIRSTRUN = 1,
+        DOWNLOAD_EARTHEND = 2
     };
 
     Action myAction;
 
+    std::string platform = "windows";
+
     Worker();
+    void platformSpecific();
     void init();
     void getAvailableServers();
     void testConnection();
     static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output, int dl_id);
     static int ProgressCallback(void* clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow);
-    std::vector<char> downloadFromUrl(std::string& url);
+    int testUrl(std::string& url);
+    std::vector<char> downloadFromUrl(std::string url, bool addToBuffer = false);
+    std::vector<char> downloadFromUrlPost(std::string url, std::vector<std::string> postParams, std::vector<std::string> postValues, bool addToBuffer = false);
     void setAction(Action action);
     void listen();
     bool isBusy();
