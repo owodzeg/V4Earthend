d664ca4e2678446a113639d9b7e79e22ebb9f2b8
owocek <owocektv@gmail.com>
2024-09-27 00:44:13 +0200

    launcher main entry wip

--- Engine/Menu/Entry.cpp
+++ Engine/Menu/Entry.cpp
@@ -37,15 +37,235 @@ void Entry::init()
         strRepo->langToFontMapping[code] = font;
     }
 
+    p_head.setFillColor(sf::Color::Black);
+    p_white.setFillColor(sf::Color::White);
+    p_pupil.setFillColor(sf::Color::Black);
+
+    sf::RenderWindow* window = CoreManager::getInstance().getWindow();
+    pon_x_c = window->getSize().x/2;
+    pon_y_c = window->getSize().y/2;
+    pon_x_d = pon_x_c;
+    pon_y_d = pon_y_c;
+
+    /*
+    r_head_d = 62;
+    r_white_d = 41;
+    r_pupil_d = 27;
+    */
+
+    r_head_d = 62 * 100;
+    r_white_d = 41 * 100;
+    r_pupil_d = 27 * 100;
+
+    r_head_c = r_head_d;
+    r_white_c = r_white_d;
+    r_pupil_c = r_pupil_d;
+
+    logo.load("resources/graphics/ui/logo.png");
+
+    logo_x_d = 3840/2;
+    logo_y_d = 2160/2;
+
+    logo_x_c = logo_x_d;
+    logo_y_c = logo_y_d;
+
+    bg.Load("earthend");
+
+    a_clock.restart();
+
     SPDLOG_INFO("Entry initialized.");
 }
 
 void Entry::draw()
 {
-    test.setFont("kaku-uni");
-    test.setCharacterSize(20);
-    test.setString("{color 255 255 255} Thank you for testing.");
-    test.setOrigin(test.getLocalBounds().width/2, test.getLocalBounds().height/2);
-    test.setPosition(3840/2, 2160/2);
-    test.draw();
+    sf::RenderWindow* window = CoreManager::getInstance().getWindow();
+    Worker* worker = CoreManager::getInstance().getWorker();
+
+    //SPDLOG_INFO("pon_x_c {} pon_x_d {} pon_x_offset_c {} pon_x_offset_d {}", pon_x_c, pon_x_d, pon_x_offset_c, pon_x_offset_d);
+    //SPDLOG_INFO("pon_y_c {} pon_y_d {} pon_y_offset_c {} pon_y_offset_d {}", pon_y_c, pon_y_d, pon_y_offset_c, pon_y_offset_d);
+
+    float fps = CoreManager::getInstance().getCore()->fps;
+    float speed_delta = speed / fps * 240;
+
+    if(fabs(r_head_c - r_head_d) > 0)
+    {
+        if(r_head_c > r_head_d)
+        {
+            r_head_c -= fabs(r_head_c - r_head_d) * speed_delta;
+        }
+        else
+        {
+            r_head_c += fabs(r_head_c - r_head_d) * speed_delta;
+        }
+    }
+    if(fabs(r_white_c - r_white_d) > 0)
+    {
+        if(r_white_c > r_white_d)
+        {
+            r_white_c -= fabs(r_white_c - r_white_d) * speed_delta;
+        }
+        else
+        {
+            r_white_c += fabs(r_white_c - r_white_d) * speed_delta;
+        }
+    }
+    if(fabs(r_pupil_c - r_pupil_d) > 0)
+    {
+        if(r_pupil_c > r_pupil_d)
+        {
+            r_pupil_c -= fabs(r_pupil_c - r_pupil_d) * speed_delta;
+        }
+        else
+        {
+            r_pupil_c += fabs(r_pupil_c - r_pupil_d) * speed_delta;
+        }
+    }
+    if(fabs(pupil_offset_x_c - pupil_offset_x_d) > 0)
+    {
+        if(pupil_offset_x_c > pupil_offset_x_d)
+        {
+            pupil_offset_x_c -= fabs(pupil_offset_x_c - pupil_offset_x_d) * speed_delta;
+        }
+        else
+        {
+            pupil_offset_x_c += fabs(pupil_offset_x_c - pupil_offset_x_d) * speed_delta;
+        }
+    }
+    if(fabs(pupil_offset_y_c - pupil_offset_y_d) > 0)
+    {
+        if(pupil_offset_y_c > pupil_offset_y_d)
+        {
+            pupil_offset_y_c -= fabs(pupil_offset_y_c - pupil_offset_y_d) * speed_delta;
+        }
+        else
+        {
+            pupil_offset_y_c += fabs(pupil_offset_y_c - pupil_offset_y_d) * speed_delta;
+        }
+    }
+    if(fabs(pupil_angle_c - pupil_angle_d) > 0)
+    {
+        if(pupil_angle_c > pupil_angle_d)
+        {
+            pupil_angle_c -= fabs(pupil_angle_c - pupil_angle_d) * speed_delta;
+        }
+        else
+        {
+            pupil_angle_c += fabs(pupil_angle_c - pupil_angle_d) * speed_delta;
+        }
+    }
+    if(fabs(pon_x_c - pon_x_d) > 0)
+    {
+        if(pon_x_c > pon_x_d)
+        {
+            pon_x_c -= fabs(pon_x_c - pon_x_d) * speed_delta;
+        }
+        else
+        {
+            pon_x_c += fabs(pon_x_c - pon_x_d) * speed_delta;
+        }
+    }
+    if(fabs(pon_y_c - pon_y_d) > 0)
+    {
+        if(pon_y_c > pon_y_d)
+        {
+            pon_y_c -= fabs(pon_y_c - pon_y_d) * speed_delta;
+        }
+        else
+        {
+            pon_y_c += fabs(pon_y_c - pon_y_d) * speed_delta;
+        }
+    }
+    if(fabs(pon_y_offset_c - pon_y_offset_d) > 0)
+    {
+        if(pon_y_offset_c > pon_y_offset_d)
+        {
+            pon_y_offset_c -= fabs(pon_y_offset_c - pon_y_offset_d) * speed_delta;
+        }
+        else
+        {
+            pon_y_offset_c += fabs(pon_y_offset_c - pon_y_offset_d) * speed_delta;
+        }
+    }
+    if(fabs(pon_x_offset_c - pon_x_offset_d) > 0)
+    {
+        if(pon_x_offset_c > pon_x_offset_d)
+        {
+            pon_x_offset_c -= fabs(pon_x_offset_c - pon_x_offset_d) * speed_delta;
+        }
+        else
+        {
+            pon_x_offset_c += fabs(pon_x_offset_c - pon_x_offset_d) * speed_delta;
+        }
+    }
+    
+    if(fabs(logo_x_c - logo_x_d) > 0)
+    {
+        if(logo_x_c > logo_x_d)
+        {
+            logo_x_c -= fabs(logo_x_c - logo_x_d) * speed_delta;
+        }
+        else
+        {
+            logo_x_c += fabs(logo_x_c - logo_x_d) * speed_delta;
+        }
+    }
+    
+    if(fabs(logo_y_c - logo_y_d) > 0)
+    {
+        if(logo_y_c > logo_y_d)
+        {
+            logo_y_c -= fabs(logo_y_c - logo_y_d) * speed_delta;
+        }
+        else
+        {
+            logo_y_c += fabs(logo_y_c - logo_y_d) * speed_delta;
+        }
+    }
+
+    if(a_state == 0 && a_clock.getElapsedTime().asMilliseconds() > 1500)
+    {
+        a_state = 1;
+        speed = 0.02;
+
+        r_head_d = 62*0.75;
+        r_white_d = 41*0.75;
+        r_pupil_d = 27*0.75;
+
+        pon_x_offset_d = 56;
+        pon_y_offset_d = 15;
+
+        a_clock.restart();
+    }
+
+    if(a_state == 1 && a_clock.getElapsedTime().asMilliseconds() > 2500)
+    {
+        speed = 0.015;
+
+        pon_y_d -= 200;
+        logo_y_d -= 600;
+
+        a_state = 2;
+    }
+
+    bg.Draw(camera);
+
+    logo.setOrigin(logo.getGlobalBounds().width/2, logo.getGlobalBounds().height/2);
+    logo.setPosition(logo_x_c, logo_y_c);
+    logo.draw();
+
+    p_head.setRadius(r_head_c);
+    p_white.setRadius(r_white_c);
+    p_pupil.setRadius(r_pupil_c);
+    p_head.setOrigin(r_head_c, r_head_c);
+    p_white.setOrigin(r_white_c, r_white_c);
+    p_pupil.setOrigin(r_pupil_c + pupil_offset_x_c, r_pupil_c + pupil_offset_y_c);
+    p_pupil.setRotation(pupil_angle_c);
+
+    p_head.setPosition(pon_x_c + pon_x_offset_c, pon_y_c + pon_y_offset_c);
+    p_white.setPosition(pon_x_c + pon_x_offset_c, pon_y_c + pon_y_offset_c);
+    p_pupil.setPosition(pon_x_c + pon_x_offset_c, pon_y_c + pon_y_offset_c);
+
+    window->draw(p_head);
+    window->draw(p_white);
+    window->draw(p_pupil);
 }
--- Engine/Menu/Entry.h
+++ Engine/Menu/Entry.h
@@ -2,11 +2,40 @@
 #define ENTRY_H
 
 #include <SFML/Graphics.hpp>
+#include "../Graphics/SpriteWrapper.h"
 #include "../Graphics/PText.h"
+#include "../Mechanics/Background.h"
 
 class Entry {
 public:
-  PText test;
+  sf::CircleShape p_head, p_white, p_pupil;
+  SpriteWrapper logo;
+
+  float pon_x_c = 0, pon_y_c = 0;
+  float pon_x_d = 0, pon_y_d = 0;
+
+  float pon_x_offset_c = 0, pon_y_offset_c = 0;
+  float pon_x_offset_d = 0, pon_y_offset_d = 0;
+
+  float r_head_c=0, r_white_c=0, r_pupil_c=0; //current
+  float r_head_d=0, r_white_d=0, r_pupil_d=0; //destination
+
+  float pupil_offset_x_c = 0, pupil_offset_x_d = 0;
+  float pupil_offset_y_c = 0, pupil_offset_y_d = 0;
+  float pupil_angle_c = 0, pupil_angle_d = 0;
+
+  float logo_x_c = 0, logo_y_c = 0;
+  float logo_x_d = 0, logo_y_d = 0;
+
+  float speed = 0.015;
+
+  sf::RectangleShape background;
+
+  sf::Clock a_clock;
+  int a_state = 0;
+
+  Camera camera;
+  Background bg;
 
   void init();
   void draw();
