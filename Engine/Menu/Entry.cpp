b3c5092ff6658f06a3d02208a93fabb0acbcb049
owocek <owocektv@gmail.com>
2024-09-27 23:47:22 +0200

    multilanguage support

--- Engine/Menu/Entry.cpp
+++ Engine/Menu/Entry.cpp
@@ -39,6 +39,11 @@ void Entry::init()
         SPDLOG_INFO("Loaded language {} {}, font file: {}", code, name, font);
     }
 
+    std::ifstream langConfig("resources/lang.txt");
+    std::string buffer;
+    std::getline(langConfig, buffer);
+    strRepo->SetCurrentLanguage(buffer);
+
     p_head.setFillColor(sf::Color::Black);
     p_white.setFillColor(sf::Color::White);
     p_pupil.setFillColor(sf::Color::Black);
@@ -84,29 +89,59 @@ void Entry::init()
     if(localTime->tm_hour >= 5 && localTime->tm_hour <= 9)
     {
         bg.Load("earthend_1");
+
+        MessageCloud tmp;
+        tmp.Create(20, sf::Vector2f(pon_greet.getGlobalPosition().x+360, pon_greet.getGlobalPosition().y), sf::Color(255, 255, 255, 255), false, 3);
+        tmp.msgcloud_ID = 0;
+        tmp.AddDialog("ln_greeting1", true);
+        messageclouds.push_back(tmp);
     }
 
     if(localTime->tm_hour >= 10 && localTime->tm_hour <= 16)
     {
         bg.Load("earthend_2");
-        pon_greet.setAnimation("Units_Patapon_Sleep");
-        pon_greet.setAnimationSpeed(0); // workaround for lack of animation stalling
-        pon_greet.setGlobalPosition(sf::Vector2f(cam_placement*3 + 230, 1612));
+        MessageCloud tmp;
+        tmp.Create(20, sf::Vector2f(pon_greet.getGlobalPosition().x+360, pon_greet.getGlobalPosition().y), sf::Color(255, 255, 255, 255), false, 3);
+        tmp.msgcloud_ID = 0;
+        tmp.AddDialog("ln_greeting2", true);
+        messageclouds.push_back(tmp);
     }
 
     if(localTime->tm_hour >= 17 && localTime->tm_hour <= 19)
     {
         bg.Load("earthend_3");
+
+        MessageCloud tmp;
+        tmp.Create(20, sf::Vector2f(pon_greet.getGlobalPosition().x+360, pon_greet.getGlobalPosition().y), sf::Color(255, 255, 255, 255), false, 3);
+        tmp.msgcloud_ID = 0;
+        tmp.AddDialog("ln_greeting3", true);
+        messageclouds.push_back(tmp);
     }
 
     if(localTime->tm_hour >= 20 && localTime->tm_hour <= 22)
     {
         bg.Load("earthend_4");
+
+        MessageCloud tmp;
+        tmp.Create(20, sf::Vector2f(pon_greet.getGlobalPosition().x+360, pon_greet.getGlobalPosition().y), sf::Color(255, 255, 255, 255), false, 3);
+        tmp.msgcloud_ID = 0;
+        tmp.AddDialog("ln_greeting4", true);
+        messageclouds.push_back(tmp);
     }
 
     if(localTime->tm_hour >= 23 || localTime->tm_hour <= 4)
     {
         bg.Load("earthend_5");
+
+        MessageCloud tmp;
+        tmp.Create(20, sf::Vector2f(pon_greet.getGlobalPosition().x+320, pon_greet.getGlobalPosition().y+45), sf::Color(255, 255, 255, 255), false, 3);
+        tmp.msgcloud_ID = 0;
+        tmp.AddDialog("ln_greeting5", true);
+        messageclouds.push_back(tmp);
+
+        pon_greet.setAnimation("Units_Patapon_Sleep");
+        pon_greet.setAnimationSpeed(0); // workaround for lack of animation stalling
+        pon_greet.setGlobalPosition(sf::Vector2f(cam_placement*3 + 230, 1612));
     }
 
     background.setSize(sf::Vector2f(1280, 720));
@@ -194,24 +229,29 @@ void Entry::draw()
         p_login.setCharacterSize(50);
         p_login.setStringKey("ln_button_login");
         p_login.setOrigin(p_login.getLocalBounds().width/2, p_login.getLocalBounds().height/2);
+        p_login.setColor(sf::Color::White);
         p_login.setPosition(3840/2, 1000);
 
         p_register.setFont(font);
         p_register.setCharacterSize(50);
         p_register.setStringKey("ln_button_register");
         p_register.setOrigin(p_register.getLocalBounds().width/2, p_register.getLocalBounds().height/2);
+        p_register.setColor(sf::Color::White);
         p_register.setPosition(3840/2, 1200);
 
         p_offline.setFont(font);
         p_offline.setCharacterSize(50);
         p_offline.setStringKey("ln_button_playoffline");
         p_offline.setOrigin(p_offline.getLocalBounds().width/2, p_offline.getLocalBounds().height/2);
+        p_offline.setColor(sf::Color::White);
         p_offline.setPosition(3840/2, 1400);
 
         auto pos = p_login.getPosition();
         auto bounds = p_login.getLocalBounds();
         if(mouse.x > pos.x-bounds.width/2 && mouse.x < pos.x+bounds.width/2 && mouse.y > pos.y-bounds.height/2 && mouse.y < pos.y+bounds.height/2)
         {
+            p_login.setColor(sf::Color::Green);
+
             sword.setOrigin(sword.getGlobalBounds().width/2, sword.getGlobalBounds().height/2);
             sword.setScale(1,1);
             sword.setPosition(pos.x - bounds.width/2 - 130 - swordOffset, pos.y + 30);
@@ -231,6 +271,8 @@ void Entry::draw()
         bounds = p_register.getLocalBounds();
         if(mouse.x > pos.x-bounds.width/2 && mouse.x < pos.x+bounds.width/2 && mouse.y > pos.y-bounds.height/2 && mouse.y < pos.y+bounds.height/2)
         {
+            p_register.setColor(sf::Color::Green);
+
             sword.setOrigin(sword.getGlobalBounds().width/2, sword.getGlobalBounds().height/2);
             sword.setScale(1,1);
             sword.setPosition(pos.x - bounds.width/2 - 130 - swordOffset, pos.y + 30);
@@ -250,6 +292,8 @@ void Entry::draw()
         bounds = p_offline.getLocalBounds();
         if(mouse.x > pos.x-bounds.width/2 && mouse.x < pos.x+bounds.width/2 && mouse.y > pos.y-bounds.height/2 && mouse.y < pos.y+bounds.height/2)
         {
+            p_offline.setColor(sf::Color::Green);
+
             sword.setOrigin(sword.getGlobalBounds().width/2, sword.getGlobalBounds().height/2);
             sword.setScale(1,1);
             sword.setPosition(pos.x - bounds.width/2 - 130 - swordOffset, pos.y + 30);
@@ -272,6 +316,8 @@ void Entry::draw()
 
                 background.setSize(sf::Vector2f(322, 720));
                 background.setPosition(cam_placement+960, 0);
+
+                CoreManager::getInstance().getGlobals()->set(1, std::string("Guest"));
             }
         }
 
@@ -316,14 +362,26 @@ void Entry::draw()
         if(mouse.x > 1100-pedestal.getGlobalBounds().width/2 && mouse.x < 1100+pedestal.getGlobalBounds().width/2 && mouse.y >= 1200 && mouse.y <= 2160)
         {
             p_active = 1;
+            if(mouseCtrl->getClick(0))
+            {
+
+            }
         }
         if(mouse.x > 1100+650-pedestal.getGlobalBounds().width/2 && mouse.x < 1100+650+pedestal.getGlobalBounds().width/2 && mouse.y >= 1200 && mouse.y <= 2160)
         {
             p_active = 2;
+            if(mouseCtrl->getClick(0))
+            {
+
+            }
         }
         if(mouse.x > 1100+650+650-pedestal.getGlobalBounds().width/2 && mouse.x < 1100+650+650+pedestal.getGlobalBounds().width/2 && mouse.y >= 1200 && mouse.y <= 2160)
         {
             p_active = 3;
+            if(mouseCtrl->getClick(0))
+            {
+                CoreManager::getInstance().getCore()->close_window = true;
+            }
         }
 
         pedestal.setOrigin(pedestal.getGlobalBounds().width/2, pedestal.getGlobalBounds().height);
@@ -352,10 +410,10 @@ void Entry::draw()
 
         sword.setOrigin(sword.getGlobalBounds().width/2, sword.getGlobalBounds().height/2);
         sword.setScale(1,1);
-        sword.setPosition(pos.x - bounds.width/2 - 180, pos.y + 25);
+        sword.setPosition(pos.x - bounds.width*3/2 - sword.getGlobalBounds().width/2, pos.y + 25);
         sword.draw();
         sword.setScale(-1,1);
-        sword.setPosition(pos.x + bounds.width/2 + 180, pos.y + 25);
+        sword.setPosition(pos.x + bounds.width*3/2 + sword.getGlobalBounds().width/2, pos.y + 25);
         sword.draw();
 
         p_news.setFont(font);
@@ -369,6 +427,7 @@ void Entry::draw()
         p_play.setStringKey("ln_button_play");
         p_play.setOrigin(p_play.getLocalBounds().width/2, p_play.getLocalBounds().height/2);
         p_play.setPosition(cam_placement*3 + 1100, 1900);
+        p_play.setColor(sf::Color(255, 255, 255));
         if(p_active == 1) { p_play.setColor(sf::Color(255, 197, 31)); }
         p_play.draw();
 
@@ -377,7 +436,8 @@ void Entry::draw()
         p_settings.setStringKey("ln_button_settings");
         p_settings.setOrigin(p_settings.getLocalBounds().width/2, p_settings.getLocalBounds().height/2);
         p_settings.setPosition(cam_placement*3 + 1100 + 650, 1900);
-        if(p_active == 1) { p_settings.setColor(sf::Color(255, 197, 31)); }
+        p_settings.setColor(sf::Color(255, 255, 255));
+        if(p_active == 2) { p_settings.setColor(sf::Color(255, 197, 31)); }
         p_settings.draw();
 
         p_exit.setFont(font);
@@ -385,7 +445,8 @@ void Entry::draw()
         p_exit.setStringKey("ln_button_exit");
         p_exit.setOrigin(p_exit.getLocalBounds().width/2, p_exit.getLocalBounds().height/2);
         p_exit.setPosition(cam_placement*3 + 1100 + 650*2, 1900);
-        if(p_active == 1) { p_exit.setColor(sf::Color(255, 197, 31)); }
+        p_exit.setColor(sf::Color(255, 255, 255));
+        if(p_active == 3) { p_exit.setColor(sf::Color(255, 197, 31)); }
         p_exit.draw();
 
         pon_menu1.setAnimation("Units_Patapon_Idle_1");
@@ -415,5 +476,25 @@ void Entry::draw()
         pon_menu1.Draw();
         pon_menu2.Draw();
         pon_menu3.Draw();
+
+        std::vector<int> m_rm;
+
+        for (ulong i = 0; i < messageclouds.size(); i++)
+        {
+            if (messageclouds[i].firstrender)
+                messageclouds[i].Show();
+
+            messageclouds[i].Draw();
+
+            if ((!messageclouds[i].active) && (messageclouds[i].done))
+                m_rm.push_back(i);
+        }
+
+        for (ulong i = 0; i < m_rm.size(); i++)
+        {
+            SPDLOG_DEBUG("Erasing MessageCloud id {}", m_rm[i]);
+            messageclouds.erase(messageclouds.begin() + m_rm[i] - i);
+        }
+
     }
 }
